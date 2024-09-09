bbc03aa1b6cfe02ed58e687039b45edff7aadcbd
tetianasvidrak <svidrak.tetiana@gmail.com>
2024-09-09 17:06:28 +0200

    feat > added basic rounting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div>So Yummy</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="signup" element={<div>SignUp</div>} />
+      <Route path="signin" element={<div>Signin</div>} />
+    </Routes>
+  );
 }
 
 export default App;
