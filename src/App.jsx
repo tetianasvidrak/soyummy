66ffc48485ab7a0b4facd6df7c8c2aee99889b56
tetianasvidrak <svidrak.tetiana@gmail.com>
2024-09-12 18:57:44 +0200

    feat > added signup logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+import SignUp from "./pages/SignUp";
+import Welcome from "./pages/Welcome";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
-      <Route path="signup" element={<div>SignUp</div>} />
+      <Route path="signup" element={<SignUp />} />
       <Route path="signin" element={<div>Signin</div>} />
     </Routes>
   );
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,24 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import * as authOperations from "../../redux/auth/authOperations";
 
 import signUpImg from "../../assets/signUpImg.png";
 import AuthForm from "../../components/AuthForm";
 
 const SignUp = () => {
+  const [signUpData, setSignUpData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   const onSubmitSignUpHandler = (data) => {
-    console.log(data);
+    setSignUpData((prevState) => ({ ...prevState, ...data }));
+    dispatch(authOperations.register({ ...signUpData, ...data }));
   };
 
   return (
@@ -25,6 +38,7 @@ const SignUp = () => {
         </div>
         <div className="absolute bottom-0 z-0 h-[380px] w-full bg-cover bg-[url('./assets/bgAuthFloor.svg')]"></div>
       </div>
+      {isLoggedIn && <Navigate to="/" />}
     </>
   );
 };
